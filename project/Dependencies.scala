50efa8a23a682d129f14d994951cb96f894ad2e3
Andriy Onyshchuk <andyglow@gmail.com>
2021-05-11 19:08:27 -0700

    get prepared for Scala3 (#12)
    * prepare for scala 3
    
    * prepare drone.io for scala 3
    
    Co-authored-by: aonyshchuk <aonyshchuk@apple.com>

---
+++ project/Dependencies.scala
@@ -0,0 +1,17 @@
+import sbt._
+
+object Dependencies {
+
+  def xml(scalaV: ScalaVer): ModuleID = {
+    val v = scalaV match {
+      case ScalaVer._211 => "1.3.0"
+      case ScalaVer._212 => "2.0.0-RC1"
+      case ScalaVer._213 => "2.0.0-RC1"
+      case ScalaVer._300 => "2.0.0-RC1"
+    }
+
+    "org.scala-lang.modules"  %% "scala-xml" % v
+  }
+
+  def scalatest = "org.scalatest" %% "scalatest" % "3.2.8" % Provided
+}
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-version in ThisBuild := "2.0.5-SNAPSHOT"
+ThisBuild / version := "2.0.5-SNAPSHOT"
